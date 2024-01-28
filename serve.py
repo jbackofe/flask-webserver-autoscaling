82b82e644cc93cae043dac1b8025025b03824ecd
Ubuntu <ubuntu@ip-172-31-8-250.us-east-2.compute.internal>
2024-01-28 21:23:41 +0000

    init commit

---
+++ serve.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify
+import socket
+import subprocess
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def get_hostname():
+    return str(socket.gethostname())
+
+@app.route('/', methods=['POST'])
+def start_process():
+    subprocess.Popen(['python', 'stress_cpu.py'])
+
+    return jsonify({"message": "started stress_cpu.py"}), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
